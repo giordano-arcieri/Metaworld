11b00996ff85bc23e75adfd0a8bfa1a6bb0e7b85
giordano-arcieri <arcierigiordano@gmail.com>
2025-06-08 19:58:00 -0400

    added files for custom env

---
+++ metaworld/new_envs/__init__.py
@@ -0,0 +1,5 @@
+from metaworld.new_envs.sawyer_two_balls import SawyerTwoBalls
+
+__all__ = [
+    "SawyerTwoBalls",
+]
---
+++ metaworld/new_envs/sawyer_two_balls.py
@@ -0,0 +1,5 @@
+
+from metaworld.sawyer_xyz_env import SawyerXYZEnv
+
+class SawyerTwoBalls(SawyerXYZEnv):
+   pass
